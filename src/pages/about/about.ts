f02cbea1fc236e1b429225cbeb5fcf0f359fd5b2
ryan-dalton <46613215+ryan-dalton@users.noreply.github.com>
2020-04-29 20:51:53 -0600

    modals

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ModalController} from 'ionic-angular';
+
 
 @Component({
   selector: 'page-about',
@@ -7,8 +9,15 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
 
   }
 
+  presentModal() {
+    const modal = this.modalCtrl.create('ModalPage');
+    modal.present();
+  }
+
 }
+
+
