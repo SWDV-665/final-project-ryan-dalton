676770f2a7816cdd0fe0e75fa9d60f8fbed1796f
ryan-dalton <46613215+ryan-dalton@users.noreply.github.com>
2020-05-01 21:18:16 -0600

    uploads functional

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -5,31 +5,38 @@ import { ViewController } from 'ionic-angular';
 import { NavParams } from 'ionic-angular';
 //import { CameraServiceProvider } from '../../providers/camera-service/camera-service';
 import { Camera, CameraOptions } from '@ionic-native/camera';
+import { JournalEntryProvider } from '../../providers/journal-entry/journal-entry';
+
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html',
 })
 export class AboutPage {
+  chosenDate;
   textEntry: String;
   public imagesource: String;
+  public itemDate: Date;
+  public title: String;
+  public blurb: String;
+  //Using photopaths to local files - web support coming in the future
+  public picture: String;
   //private win: any = window;
  
   constructor(public navCtrl: NavController, 
               public modalCtrl: ModalController, 
-              public viewCtrl: ViewController, 
+              public viewCtrl: ViewController,
               public navParams: NavParams,
-              public camera: Camera
-              
+              public camera: Camera,
+              public journalService: JournalEntryProvider
               ) {
-              
-              this.textEntry = null;
-              this.imagesource = "assets/imgs/photo-icon.png" 
-              
+               
   }
 
   ionViewDidEnter(){
-    //Refresh
+    this.textEntry = "Enter info here";
+    this.imagesource = "assets/imgs/photo-icon.png";
+    this.picture = "assets/imgs/photo-icon.png";
   }
 
   checkAllEntries() {
@@ -43,6 +50,7 @@ export class AboutPage {
     myModal.onDidDismiss((textEntry) => {
       console.log("brought data", textEntry, "out of ModalPage");
       this.loadModalContent(textEntry);
+      this.blurb = textEntry;
     })
 
   }
@@ -63,10 +71,25 @@ export class AboutPage {
   
     this.camera.getPicture(options).then((imageData) => {
       this.imagesource = imageData; //'data:image/jpeg/base64,' + 
+      this.picture = imageData;
       console.log("Added image URI: ", imageData)
     }, (err)=> {
       console.log(err);
     });
+  }
+
+  buttonClicked(){
+    let data = {
+      "title": this.title,
+      "itemDate": this.chosenDate,
+      "picture": this.picture,
+      "blurb": this.blurb
+    }
+    console.log("Captured data:", data);
+    this.journalService.addEntry(data);
+  }
+  assembleEntry(){
+    return 0;    
   }
 
   }
